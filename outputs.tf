37ee29d46c1bcd477398120773cad0232b89937a
cantte <cantte098@gmail.com>
2023-11-29 16:15:15 -0500

    Update API Gateway URL in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "base_url" {
   description = "Base URL for API Gateway stage."
 
-  value = aws_apigatewayv2_stage.songs_api.invoke_url
+  value = aws_apigatewayv2_stage.deliveries_api.invoke_url
 }
